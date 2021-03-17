c74b927a2854e74f19038177058073f65930bfa0
m-wiemers <marcel@klebauter.de>
2021-03-17 19:09:36 +0100

    💄 Add Dropdownmenu for Start- and End-City

---
+++ components/citieSelect/SelectCity.tsx
@@ -0,0 +1,33 @@
+import { ChangeEventHandler } from "react";
+import styles from "./SelectCity.module.css";
+
+export type SelectProps = {
+  label: string;
+  value: string;
+  onSelect: ChangeEventHandler<HTMLSelectElement>;
+};
+
+function SelectCity({ label, value, onSelect }: SelectProps) {
+  return (
+    <div className={styles.container}>
+      <label className={styles.label}>{label}</label>
+      <select
+        className={styles.cityselect}
+        name="StartCity"
+        onChange={onSelect}
+      >
+        <option className={styles.option} value={value}>
+          {value[0]}
+        </option>
+        <option className={styles.option} value={value}>
+          {value[1]}
+        </option>
+        <option className={styles.option} value={value}>
+          {value[2]}
+        </option>
+      </select>
+    </div>
+  );
+}
+
+export default SelectCity;
