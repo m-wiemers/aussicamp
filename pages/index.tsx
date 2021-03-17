9cf9909e4ed77b8a0f410263f7957051bb13ad1e
m-wiemers <marcel@klebauter.de>
2021-03-17 17:27:10 +0100

    ⚰️ remove and clean index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Button from "../components/button/Button";
-import Logo from "../components/logo/Logo";
-import SearchInput from "../components/input/SearchInput";
-import DateInput from "../components/DateInput/DateInput";
 
 export default function Home() {
   return (
@@ -13,12 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Button label="HERE WE GO" />
-        <Logo subTitle={true} />
-        <SearchInput />
-        <DateInput label="When does it start?" />
-      </main>
+      <main className={styles.main}></main>
 
       <footer className={styles.footer}></footer>
     </div>
