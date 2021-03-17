ada14f93fb0eb6a4cc9afcfffd2e3671f62acaf3
m-wiemers <marcel@klebauter.de>
2021-03-17 18:12:51 +0100

    🏷️ Add onClick to Button

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -1,13 +1,15 @@
+import { MouseEventHandler } from "react";
 import ArrowRightIcon from "../icons/ArrowRightIcon";
 import styles from "./Button.module.css";
 
 export type ButtonProps = {
   label: string;
+  onButtonClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-function Button({ label }: ButtonProps) {
+function Button({ label, onButtonClick }: ButtonProps) {
   return (
-    <button className={styles.btn}>
+    <button className={styles.btn} onClick={onButtonClick}>
       {label}
       <span className={styles.icon}>
         <ArrowRightIcon />
