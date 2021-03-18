292b060862772fbfcc5009dba1d5c47167d3c534
m-wiemers <marcel@klebauter.de>
2021-03-18 15:19:17 +0100

    💄Style and add Header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/header/Header";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
       <main className={styles.main}></main>
 
       <footer className={styles.footer}></footer>
