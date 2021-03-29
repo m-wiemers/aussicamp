05df9bd558179ebea1b999b16b7b3fbbfe2af391
m-wiemers <marcel@klebauter.de>
2021-03-29 11:44:49 +0200

    💄 refactor LinkButton

---
+++ components/button/LinkButton.stories.tsx
@@ -0,0 +1,22 @@
+import { Story } from "@storybook/react/types-6-0";
+import React from "react";
+import LinkButton, { LinkButtonProps } from "./LinkButton";
+
+export default {
+  title: "Common/Button",
+  component: LinkButton,
+};
+
+const Template: Story<LinkButtonProps> = (args) => <LinkButton {...args} />;
+
+export const HereWeGoButton = Template.bind({});
+HereWeGoButton.args = {
+  label: "HERE WE GO",
+  link: "#",
+};
+
+export const ContinueButton = Template.bind({});
+ContinueButton.args = {
+  label: "continue",
+  link: "#",
+};
