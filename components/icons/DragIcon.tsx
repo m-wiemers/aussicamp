331d5afeaa020394440299b3272b6c63deb73930
m-wiemers <marcel@klebauter.de>
2021-03-29 12:06:00 +0200

    ⚰️ remove dead Code from DragIcon

--- components/icons/DragIcon.tsx
+++ components/icons/DragIcon.tsx
@@ -5,8 +5,8 @@ const DragIcon = () => (
     height="39"
     viewBox="0 0 42 39"
   >
-    <g id="sort" transform="translate(-307 -127)">
-      <path d="M42,39h0Z" transform="translate(307 108)" opacity="0" />
+    <g transform="translate(-307 -127)">
+      <path d="M42,39h0Z" transform="translate(307 108)" />
       <path
         d="M4.5,27h27V24H4.5Zm0-7.5h27v-3H4.5ZM4.5,9v3h27V9Z"
         transform="translate(310.5 129)"
